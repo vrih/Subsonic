f36d1d8593d84940ccd89e05b5c3db474cc38e8e
Daniel Bowman <daniel@danielbowman.co.uk>
2018-12-16 14:50:07 +0000

    Migrate to androidx

--- app/src/main/java/github/daneren2005/dsub/util/Notifications.java
+++ app/src/main/java/github/daneren2005/dsub/util/Notifications.java
@@ -26,14 +26,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Handler;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.RemoteViews;
-import android.widget.TextView;
 
 import github.daneren2005.dsub.R;
 import github.daneren2005.dsub.activity.SubsonicActivity;
